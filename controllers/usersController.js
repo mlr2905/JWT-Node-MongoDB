c3467dd18ba4bd6038241b801c2e59360c2492cd
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-05-08 12:43:35 +0300

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -33,7 +33,12 @@ const handleErrors = (err) => {
 }
 
 // create json web token
-
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id, email) => {
+    return jwt.sign({ id, email }, 'secret key', {
+        expiresIn: maxAge
+    });
+};
 
 module.exports.signup_post = async (request, response) => {
   const { email, password } = request.body;
