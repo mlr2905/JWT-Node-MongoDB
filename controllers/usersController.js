e7eef3c4505f87ffe4f3c9e8ac221924dcfb1a84
mlr2905 <22734217+mlr2905@users.noreply.github.com>
2024-05-13 09:04:01 +0300

    up

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -78,6 +78,10 @@ const createToken = (id, email) => {
 
 module.exports.signup_post = async (request, response) => {
   const { email, password } = request.body;
+  const cipher = crypto.createCipher('aes-256-cbc', 'ml7585474rl');
+  let encryptedPassword = cipher.update(password, 'utf8', 'hex');
+  encryptedPassword += cipher.final('hex');
+  password = encryptedPassword;
   const username = email.substring(0, email.indexOf('@'));
   try {
     console.log('mongo email, password', email, password);
--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,15 +15,6 @@ const userSchema = new mongoose.Schema({
     minlength: [6, 'Minimum password length is 6 characters'],
   }
 }, { versionKey: false }); // Adding versionKey: false to prevent __v field addition
-
-// Fire a function before doc saved to db
-userSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
-});
 
 const User = mongoose.model('User', userSchema);
 
