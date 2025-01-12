c565ccc7ce0f9cd66147b2b4902b79c1dcef32bc
mlr2905 <7585474@gmail.com>
2025-01-12 17:09:48 +0200

    up

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const router = Router();
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../static/404.html'));
+    res.sendFile(path.join(__dirname, '../static/500.html'));
 });
 
 router.post('/signup', usersController.signup_post);
@@ -18,6 +18,9 @@ router.get('/search', usersController.search_users)
 router.post('/authcode', usersController.authcode)
 router.post('/verifyCode', usersController.verifyCode)
 
+router.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '../static/404.html'));
+});
 // router.get('/', usersController.encrypt_and_update_all_passwords)
 
 // router.post('', usersController.post)
